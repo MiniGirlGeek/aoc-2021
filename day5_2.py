9db4cac6ce58c160095abeff6147d3415a79c4c9
Amy Mather <amy@Amys-MacBook-Pro.local>
2021-12-05 17:29:56 +0000

    fix file name typo

--- day5_2.py
+++ day5_2.py
@@ -6,18 +6,12 @@ class Line:
 
 	def stringProcessing(self, string):
 		start, end = string[:-1].split(' -> ')
-		x1, y1 = [int(num) for num in start.split(",")]
-		x2, y2 = [int(num) for num in end.split(",")]
-		Line.max_x = max([Line.max_x, x1, x2])
-		Line.max_y = max([Line.max_y, y1, y2])
-		self.horizontal = (y1 == y2)
-		self.vertical = (x1 == x2)
-
-		self.x1 = min([x1, x2])
-		self.x2 = max([x1, x2])
-		self.y1 = min([y1, y2])
-		self.y2 = max([y1, y2])
-		# print(f'({self.x1},{self.y1}) ({self.x2},{self.y2})')
+		self.x1, self.y1 = [int(num) for num in start.split(",")]
+		self.x2, self.y2 = [int(num) for num in end.split(",")]
+		Line.max_x = max([Line.max_x, self.x1, self.x2])
+		Line.max_y = max([Line.max_y, self.y1, self.y2])
+		self.horizontal = (self.y1 == self.y2)
+		self.vertical = (self.x1 == self.x2)
 
 f = open("day5.txt", "r")
 lines = [Line(line) for line in f]
@@ -30,23 +24,38 @@ for y in range(Line.max_y + 1):
 danger_points = set()
 for line in lines:
 	if line.horizontal:
-		for x in range(line.x1, line.x2 + 1):
+		no_steps = abs(line.x2 - line.x1)
+		step = (line.x2 - line.x1) // no_steps
+		x = line.x1
+		for i in range(no_steps + 1):
 			grid[(x, line.y1)] += 1
 			if grid[(x, line.y1)] >= 2:
 				danger_points.add((x, line.y1))
-	if line.vertical:
-		for y in range(line.y1, line.y2 + 1):
+			x += step
+	elif line.vertical:
+		no_steps = abs(line.y2 - line.y1)
+		step = (line.y2 - line.y1) // no_steps
+		y = line.y1
+		for i in range(no_steps + 1):
 			grid[(line.x1, y)] += 1
 			if grid[(line.x1, y)] >= 2:
 				danger_points.add((line.x1, y))
+			y += step
+	else:
+		no_x_steps = abs(line.x2 - line.x1)
+		x_step = (line.x2 - line.x1) // no_x_steps
+		x = line.x1
 
-'''for y in range(Line.max_y + 1):
-	row = []
-	for x in range(Line.max_x + 1):
-		row.append(str(grid[(x, y)]))
-	print(" ".join(row))'''
+		no_y_steps = abs(line.y2 - line.y1)
+		y_step = (line.y2 - line.y1) // no_y_steps
+		y = line.y1
 
+		for i in range(no_x_steps + 1):
+			grid[(x, y)] += 1
+			if grid[(x, y)] >= 2:
+				danger_points.add((x, y))
+			x += x_step
+			y += y_step
 
-print(danger_points)
 print(len(danger_points))
 
