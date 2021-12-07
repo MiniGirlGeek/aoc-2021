55cdd0fe2978f0e0ec5daeaf51552228d59721ad
Amy Mather <amy@Amys-MacBook-Pro.local>
2021-12-07 16:41:10 +0000

    day 7

---
+++ day7_1.py
@@ -0,0 +1,11 @@
+import math
+f = open("day7.txt", "r")
+
+crabs = [int(crab) for crab in f.readline().split(',')]
+max_crab_pos = max(crabs)
+min_fuel_cost = math.inf
+for i in range(max_crab_pos + 1):
+	fuel_cost = sum([abs(i - pos) * crabs.count(pos) for pos in range(max_crab_pos + 1)])
+	min_fuel_cost = min(min_fuel_cost, fuel_cost)
+
+print(min_fuel_cost)
